c23b82280a19e95c72c012c5af2e20033948a60c
ZhenglinYu <buptyuzhenglin@gmail.com>
2014-11-27 17:32:33 -0500

    updating

--- Substring with Concatenation of All Words.py
+++ Substring with Concatenation of All Words.py
@@ -13,16 +13,13 @@ class Solution:
     # @param L, a list of string
     # @return a list of integer
     def findSubstring(self, S, L):
-        lenS = len(S); lenL = len(L); lenWord = len(L[0])
+        lenS = len(S); n = len(L); lenWord = len(L[0])
         result = []  #因为都是相同的长度，所以直接按照该长度来分割成array就好。
-        for start in range(lenS - lenWord * lenL + 1):
-            listS = [S[i : i+lenWord] for i in range(start, start + lenL * lenWord  , lenWord) ] # split [start, start+n*Word] to n words
-            found = True
+        for start in range(lenS - lenWord * n + 1):
+            listS = [S[i : i+lenWord] for i in range(start, start + n * lenWord  , lenWord) ] # split [start, start+n*Word] to n words
             for item in L:
                 if item in listS:
                     listS.remove(item)
-                else:
-                    found = False
-                    break
-            if found:result.append(start)
+                else:    break
+            if listS == []:result.append(start)
         return result
