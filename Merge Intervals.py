6846c7be04541ef77bed16fea7972953c5697399
ZhenglinYu <buptyuzhenglin@gmail.com>
2014-12-03 18:04:36 -0500

    updating better solution

--- Merge Intervals.py
+++ Merge Intervals.py
@@ -1,3 +1,13 @@
+'''
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+'''
+
+
+
 # Definition for an interval.
 # class Interval:
 #     def __init__(self, s=0, e=0):
@@ -6,18 +16,15 @@
 class Solution:
     # @param intervals, a list of Interval
     # @return a list of Interval
-    def merge(self, intervals):
-        if len(intervals) == 0:
-            return intervals
-        intervals.sort(key = lambda x: x.start)
-        result = [intervals[0]]
-        for i in range(1, len(intervals)):
-            current, prev = intervals[i], result[-1]
-            if current.start <= prev.end:
-                prev.end = max(prev.end, current.end)
-            else:
-                result.append(current)
-        return result
+    def merge(self, arr):
+        if len(arr) == 0:   return arr
+        arr.sort(key = lambda x: x.start)
+        ret = [arr[0]]
+        for i in range(1, len(arr)):
+            cur, pre = arr[i], ret[-1]
+            if cur.start <= pre.end:  pre.end = max(pre.end, cur.end)
+            else:   ret.append(cur)
+        return ret
 
 
 # NlogN 效率比以前高多了
