6206930fb4c7279dfff53a1284eb265e8c16be31
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-11 19:17:30 -0800

    clean the code a little bit
    clean the code a little bit

--- Remove Duplicates from Sorted List II.py
+++ Remove Duplicates from Sorted List II.py
@@ -17,12 +17,11 @@ class Solution:
     # @param head, a ListNode
     # @return a ListNode
     def deleteDuplicates(self, head):     #当head可能被删除, 不知道第一个node是哪个。 一般就用dummy。 retuen dummy.next
-        if not head or not head.next: return head
         dummy = ListNode(0)
         pre, dummy.next, cur = dummy, head, head
         while cur:
             while cur.next and cur.next.val == cur.val: cur = cur.next
             if cur != pre.next: pre.next = cur.next   #有移动的，要删除.  并且可能要连续删掉，所以不移动pre
-            else: pre = pre.next    #没有移动的，不用删除。
+            else: pre = cur    #没有移动的，不用删除。
             cur = cur.next
         return dummy.next
