234181a6b35da73acac8e1232e594e9ebca793fb
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-25 13:37:33 -0700

    Add comments
    Add comments

--- Count Primes.py
+++ Count Primes.py
@@ -7,9 +7,9 @@ class Solution:
     # @param {integer} n
     # @return {integer}
     def countPrimes(self, n):
-        if n<2: return 0
+        if n<2: return 0  #从i*i开始, 因为小于i的prime数之前已经考虑过了
         noprimes = set(j for i in range(2, int(n**0.5)+1) for j in range(i*i, n+1, i))  #来自sieve Prime.py
-        return n-len(noprimes)-2
+        return n-len(noprimes)-2   #除去1, n
 
 '''
 class Solution:
