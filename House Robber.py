7622d38295c30945c75cd86d5bcf31a40d400393
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-25 07:41:47 -0700

    Added comments
    Added comments

--- House Robber.py
+++ House Robber.py
@@ -23,11 +23,10 @@ class Solution:
     # @param num, a list of integer
     # @return an integer
     def rob(self, num):
-        size = len(num)
-        dp = [0] * (size + 1)
-        if size:
-            dp[1] = num[0]
-        for i in range(2, size + 1):
-            dp[i] = max(dp[i - 1], dp[i - 2] + num[i - 1])
-        return dp[size]
+        n = len(num)
+        dp = [0] * (n + 1)
+        if n:   dp[1] = num[0]
+        for i in range(2, n + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + num[i - 1])   #转移方程有点像maxmum subarray.
+        return dp[-1]
 '''
