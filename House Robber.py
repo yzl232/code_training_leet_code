02c4c50acecaec673e5952f37cb159d954c6da8c
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-25 07:33:36 -0700

    New problem
    New problem

---
+++ House Robber.py
@@ -0,0 +1,33 @@
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Credits:
+Special thanks to @ifanchu for adding this problem and creating all test cases. Also thanks to @ts for adding additional test cases.
+'''
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def rob(self, num):
+        size = len(num)
+        odd, even = 0, 0
+        for i in range(size):
+            if i % 2:
+                odd = max(odd + num[i], even)
+            else:
+                even = max(even + num[i], odd)
+        return max(odd, even)
+'''
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def rob(self, num):
+        size = len(num)
+        dp = [0] * (size + 1)
+        if size:
+            dp[1] = num[0]
+        for i in range(2, size + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + num[i - 1])
+        return dp[size]
+'''
