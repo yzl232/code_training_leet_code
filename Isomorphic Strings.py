3b12246d13519b7e0367c00dce39b15907a264d9
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-25 16:12:14 -0700

    New problem
    New problem

---
+++ Isomorphic Strings.py
@@ -0,0 +1,31 @@
+'''
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+For example,
+Given "egg", "add", return true.
+
+Given "foo", "bar", return false.
+
+Given "paper", "title", return true.
+
+Note:
+You may assume both s and t have the same length.
+'''
+class Solution:   #特别需要例子, 不然看着都糊涂.
+    # @param {string} s
+    # @param {string} t
+    # @return {boolean}
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t): return False
+        sourceMap, targetMap = {}, {}
+        for x in range(len(s)):
+            source, target = sourceMap.get(t[x]), targetMap.get(s[x])
+            if not source and not target:
+                sourceMap[t[x]], targetMap[s[x]] = s[x], t[x]
+            elif target != t[x] or source != s[x]:
+                return False
+        return True
