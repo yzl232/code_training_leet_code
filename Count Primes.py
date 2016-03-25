5c24efc242b2ba18773807e924e83102d1fd056e
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-25 13:31:24 -0700

    New problem
    New problem

---
+++ Count Primes.py
@@ -0,0 +1,30 @@
+'''
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+'''
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def countPrimes(self, n):
+        if n<2: return 0
+        noprimes = set(j for i in range(2, int(n**0.5)+1) for j in range(i*i, n+1, i))  #来自sieve Prime.py
+        return n-len(noprimes)-2
+
+'''
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def countPrimes(self, n):
+        isPrime = [True] * max(n, 2)
+        isPrime[0], isPrime[1] = False, False
+        x = 2
+        while x * x < n:
+            if isPrime[x]:
+                p = x * x
+                while p < n:
+                    isPrime[p] = False
+                    p += x
+            x += 1
+        return sum(isPrime)
+'''
