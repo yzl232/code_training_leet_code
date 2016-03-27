c1dd59e90ae6fefe3896f4dbbc95c1d2ff8a923c
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-27 10:16:47 -0700

    New problem
    New problem

---
+++ Summary Ranges.py
@@ -0,0 +1,13 @@
+'''
+ Given a sorted integer array without duplicates, return the summary of its ranges.
+
+For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"]. 
+'''
+
+class Solution(object):
+    def summaryRanges(self, nums):
+        ranges = []
+        for n in nums:
+            if not ranges or n > ranges[-1][-1] + 1:  ranges.append([])
+            ranges[-1][1:] = [n]
+        return ['->'.join(map(str, r)) for r in ranges]
