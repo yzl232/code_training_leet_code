3db53d209fe400bfc69ff4d63794daa17700c7f1
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-27 15:39:43 -0700

    New problems
    New problems

---
+++ Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root in (None, p, q): return root
+        l, r = self.lowestCommonAncestor(root.left, p, q), self.lowestCommonAncestor(root.right, p, q)
+        return root if (l and r) else (l if l else r)
