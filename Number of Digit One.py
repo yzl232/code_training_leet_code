cdf1f1210b7c48e349ffdc1de8c9cb11f9f80c68
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-27 14:40:48 -0700

    Add problem
    Add problem

--- Number of Digit One.py
+++ Number of Digit One.py
@@ -7,9 +7,25 @@ Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
 '''
 class Solution(object):  # http://blog.csdn.net/xudli/article/details/46798619
     def countDigitOne(self, n):  # http://www.cnblogs.com/grandyang/p/4629032.html
-        ret, base = 0, 1    #从个位到十位到百位。  m不断乘以10
-        while base <= n:  # 用(x+8)/10来判断一个数是否大于等于2
-            a, b = n/base, n%base
-            ret += (a + 8) / 10 * base + (a % 10 == 1) * (b + 1)  #判断某一位的一, 整天看,  一个数量位. 结果为x/10数量级
-            base *= 10
+        ret, base = 0, 1
+        while base<=n:
+            a, b = a/base, a%base
+            ret += (a+8)/10*base + (a%10==1)*(b+1)
+            base *=10
         return ret
+'''
+
+xxx0xx
+xxx1xx
+xxx2xx
+digit >=2:    (a/10 + 1) * base
+digit ==1:    a/10*base + (b+1)         #这里b+1<base
+digit ==0:   a/10*base
+
+
+xxx0
+xxx1
+xxx2
+base = 1
+
+'''
