187b33900ed0dff94ad6459bc9bddf203480e634
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-29 10:22:08 -0700

    Cleaner solution
    Cleaner solution

--- Word Break II.py
+++ Word Break II.py
@@ -41,3 +41,18 @@ class Solution:
         return self.ret
 
 # 可以用trie预处理
+
+'''
+def wordBreak(self, s, wordDict):
+    memo = {len(s): ['']}
+    def sentences(i):
+        if i not in memo:
+            memo[i] = [s[i:j] + (tail and ' ' + tail)
+                       for j in range(i+1, len(s)+1)
+                       if s[i:j] in wordDict
+                       for tail in sentences(j)]
+        return memo[i]
+    return sentences(0)
+
+
+'''
