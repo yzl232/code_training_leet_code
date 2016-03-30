e9eca0a7dd425a73265126a35799d97cf0e11e5f
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-30 05:07:56 -0700

    New problems
    New problems

---
+++ Find the Duplicate Number.py
@@ -0,0 +1,27 @@
+'''
+ Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Note:
+
+    You must not modify the array (assume the array is read only).
+    You must use only constant, O(1) extra space.
+    Your runtime complexity should be less than O(n2).
+    There is only one duplicate number in the array, but it could be repeated more than once.
+
+'''
+#1....n的数。 自然有特点。。。可以作为index。 
+'''
+The main idea is the same with problem Linked List Cycle II,https://leetcode.com/problems/linked-list-cycle-ii/. Use two pointers the fast and the slow. The fast one goes forward two steps each time, while the slow one goes only step each time. They must meet the same item when slow==fast. In fact, they meet in a circle, the duplicate number must be the entry point of the circle when visiting the array from nums[0]. Next we just need to find the entry point. We use a point(we can use the fast one before) to visit form begining with one step each time, do the same job to slow. When fast==slow, they meet at the entry point of the circle. The easy understood code is as follows.
+'''
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        slow = fast = finder = 0#从0开始走。 因为一直不会碰到0.  
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                while finder != slow:
+                    finder = nums[finder]
+                    slow = nums[slow]
+                return finder
---
+++ Inorder Successor in BST.py
@@ -0,0 +1,102 @@
+'''
+Problem Description:
+
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+Note: If the given node has no in-order successor in the tree, return null.
+
+'''
+#做过的。
+
+
+# encoding=utf-8
+'''
+You need to find the inorder successor and predecessor of a given key. In case the given key is not found in BST, then return the two values within which this key will lie.
+
+无所谓哪种traversal都可以做。
+'''
+
+#其实有类似于binary search的方法。  是iterative的
+#O(logN)实际上就可以做.  找一个，可以logN。类似binary search
+class Solution:
+    def successor(self, root, target):   # binary search 的思想.  binary search val
+        ret = None
+        while root:
+            if root.val>target:
+                ret = root.val
+                root=root.left
+            else:  root=root.right
+        return ret
+
+    def predessor(self, root, target):
+        ret = None
+        while root:
+            if root.val>target:  root=root.left
+            else:
+                ret = root.val
+                root=root.right
+        return ret
+
+
+
+
+
+
+
+
+'''
+class Solution:
+    def findPreSuc(self, root, val):
+        self.suc =self.prev =  None
+        self.x = val
+        self.dfs(root)
+        return self.suc, self.prev
+
+    def dfs(self, root):
+        if not root: return
+        if root.val == self.x:
+            if root.left:
+                t = root.left
+                while t.right:  t = t.right
+                self.prev = t
+            if root.right:
+                t = root.right
+                while t.left:  t = t.left
+                self.suc = t
+        elif root.val>self.x:
+            self.suc = root #太大了。 用h压一压。   和binary search一个思路
+            self.dfs(root.left)
+        else:             #注意这个elif , else  ，  就是binary  search.
+            self.prev = root
+            self.dfs(root.right)
+#基本上就是binary search
+
+
+def bs(arr, x):
+    l=0; h=len(arr)-1
+    while l<=h:
+        m = (l+h)/2
+        if m==x: return x
+        elif m<x: l=m+1
+        else: h=m-1
+'''
+
+
+
+#inorder就可以做了.   不过是O(n)。 相当于暴力解法了。 用了2个指针
+class Solution4:
+    def findSucPre(self, root, v):
+        self.pre = None; self.suc=None
+        self.dfs(root, v)
+        return self.dfs(root)
+
+    def dfs(self, root, v):
+        if not root: return
+        self.dfs(root.left, v)
+        if root.val>v:
+            if not self.suc: self.suc=root.val
+            # self.suc = min(self.suc, root.val) 这一句可以略去。 因为是从最小到最大。
+        elif root.val<v:
+            if not self.pre: self.pre=root.val
+            self.pre = max(self.pre, root.val)
+        self.dfs(root.right, v)
