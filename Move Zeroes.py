2a0ef05c06049a26e5d8eef747b280599f09076a
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-30 03:32:09 -0700

    New problems
    New problems

--- Move Zeroes.py
+++ Move Zeroes.py
@@ -1,16 +1,3 @@
-'''
- Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
-
-Note:
-
-    You must do this in-place without making a copy of the array.
-    Minimize the total number of operations.
-
-'''
-# 做过。 保持顺序。
-
 # encoding=utf-8
 '''
 http://www.geeksforgeeks.org/move-zeroes-end-array/
@@ -36,9 +23,8 @@ class Solution:
             if arr[fast]==0: continue
             arr[slow] = arr[fast]
             slow+=1
-        while slow<n:
-            arr[slow]=0
-            slow+=1
+        for i in range(slow, n):
+            arr[i] = 0
         return arr
 s = Solution()
 print(s.pushZeroesToEnd([1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9]))
---
+++ Peeking Iterator.py
@@ -0,0 +1,86 @@
+'''
+Given an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator that support the peek() operation -- it essentially peek() at the element that will be returned by the next call to next().
+
+Here is an example. Assume that the iterator is initialized to the beginning of the list: [1, 2, 3].
+
+Call next() gets you 1, the first element in the list.
+
+Now you call peek() and it returns 2, the next element. Calling next() after that still return 2.
+
+You call next() the final time and it returns 3, the last element. Calling hasNext() after that should return false.
+
+
+'''
+
+
+
+
+# encoding=utf-8
+#Implement Iterator class with peek() functionality in Java.
+#出现好几次
+'''
+那题在板上说过，一个iterator，有next和hasNext，现在要你加个wrapper，使得
+支持peek操作
+'''
+#非常高频的一道题目
+
+# 提前一步
+class IteratorPeek: #代码不长。可以背下
+    def __init__(self, iterator):
+        self.iter = iterator
+        self.top = self.getTop()
+
+    def hasNext(self):
+        return self.top!=None
+
+    def next(self):
+        ret = self.top   # self. 设置空很关键.
+        self.top = self.getTop()    #提前一步。 注意是自己的iter   #更新一个新的top
+        return ret
+
+    def getTop(self):
+        return None if not self.iter.hasNext else self.iter.next()
+
+    def peek(self):
+        return self.top
+
+
+'''
+1.写一个Stream的interface, 就是有generic, 有peek(), next(), hasNext(), append()方法. 然后写一个merge List of sorted stream, 就是的k-way merge. 然后因为是generic, 传入参数要包括一个comparator.
+
+'''
+import heapq
+class IteratorMerge: #代码不长。可以背下
+    def __init__(self, arrs):
+        self.arrs = arrs
+        self.h =[(arrs[i][0], i, 0) for i in range(len(arrs)) if arrs[i]]
+        heapq.heapify(self.h)
+
+    def hasNext(self):
+        return self.h!=None
+
+    def next(self):
+        if not self.hasNext():   raise ValueError("End of iterator")
+        val, i, j = heapq.heappop(self.h)
+        if j+1<len(self.arrs[i]):  heapq.heappush(self.h, (self.arrs[i][j+1], i, j+1))
+        return val
+
+# 像这种iterator都是那种把while 循环 分拆成一步一步的。  只要把while循环的几句抄到Next即可。
+# while的条件就是 has Next
+
+#和下面的k way merge一模一样。。。
+
+
+'''
+class Solution:
+    # @param a list of ListNode
+    # @return a ListNode
+    def mergeKLists(self, arrs):
+        h =[(arrs[i][0], i, 0) for i in range(len(arrs)) if arrs[i]]
+        heapq.heapify(h);  ret = []
+        while h:
+            val, i, j = heapq.heappop(h)
+            ret.append(val)
+            if j+1<len(arrs[i]):  heapq.heappush(h, (arrs[i][j+1], i, j+1))
+        return ret   #复杂度 O(nkLogk) 是最优解
+'''
