f8236846db27dc7f63c5f71efcc25f2a1b3c2b21
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-31 19:50:27 -0700

    New problems
    New problems

---
+++ Binary Tree Longest Consecutive Sequence.py
@@ -0,0 +1,55 @@
+'''
+Given a binary tree, find the length of the longest consecutive sequence path.
+
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+
+For example,
+
+   1
+    \
+     3
+    / \
+   2   4
+        \
+         5
+
+Longest consecutive sequence path is 3-4-5, so return 3.
+
+   2
+    \
+     3
+    / 
+   2    
+  / 
+ 1
+
+Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+'''
+
+
+class Solution:
+    def longestConsecutive(self, root):
+        self.ret = 0
+        self.dfs(root, 0, root.val)
+        return self.ret
+    
+    def dfs(self, root, cur, target):
+        if not root: return 
+        cur = 1 if root.val != target else cur+1
+        self.ret = max(self.ret, cur)
+        self.dfs(root.left, cur, root.val + 1)
+        self.dfs(root.right, cur, root.val + 1)
+        
+'''
+class Solution:
+    def longestConsecutive(self, root):
+        if not root:    return 0
+        ret = 0;  stack = [(root, 1)]
+        while stack:
+            node, cnt = stack.pop()
+            if node.left:    stack.append((node.left, cnt+1 if node.left.val == node.val + 1 else 1))
+            if node.right:   stack.append((node.right, cnt+1 if node.right.val == node.val + 1 else 1))
+            ret = max(ret, cnt)
+        return ret
+
+'''
---
+++ Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,85 @@
+'''
+
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+For example, you may serialize the following tree
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+
+as "[1,2,3,null,null,4,5]", just the same as how LeetCode OJ serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless. 
+
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    def serialize(self, root):
+        self.ret = []  #就是preorder加上特殊符号(none node)而已
+        self.dfs(root)
+        return " ".join(self.ret)#要加上空格来join
+
+    def dfs(self, root):
+        if not root:
+            self.ret.append('#')
+            return
+        self.ret.append(str(root.val))
+        self.dfs(root.left)
+        self.dfs(root.right)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        self.vals = iter(data.split())
+        return self.helper()
+
+    def helper(self):
+        x = next(self.vals, None)   # if not x: return 
+        if x=="#": return
+        root = TreeNode(x)
+        root.left = self.helper()
+        root.right = self.helper()
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
+'''
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        self.index = 0; self.arr = data.split()
+        return self.helper()
+
+    def helper(self):
+        if self.index>=len(self.arr): return
+        rootVal = self.arr[self.index]
+        self.index+=1
+        if rootVal == '#':    return
+        root = TreeNode(rootVal)  #尝试从pre-order+符号来 还原
+        root.left = self.helper()
+        root.right = self.helper()
+        return root
+
+'''
