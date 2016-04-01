f3d9c2ca10ad051b2a73f4d1f8f58528af045fb0
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-04-01 08:50:58 -0700

    New problem
    New problem

---
+++ Longest Increasing Subsequence.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):# O(n2)
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i < j:
+                m = (i + j) / 2
+                if x > tails[m]:     i = m + 1
+                else:       j = m      # 寻找需要更新的地方. x是最小的长度为i+1的ending
+            tails[i] = x
+            size = max(i + 1, size)
+        return size
+'''
+class Solution(object):
+    def lengthOfLIS(self, arr):# O(n2)
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not arr: return 0
+        ret = 1;  n = len(arr)
+        dp = [1 for i in range(n)]
+        for i in range(1, n):
+            dp[i] = max([1+dp[j] for j in range(i) if arr[j]<arr[i]] +[1])
+            ret = max(ret, dp[i])
+        return ret
+'''
