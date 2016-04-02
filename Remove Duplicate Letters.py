a3eebb1ab66a0a64621c54666b57fca850b9c4d7
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-04-02 15:26:46 -0700

    new problem
    new problem

---
+++ Remove Duplicate Letters.py
@@ -0,0 +1,39 @@
+'''
+ Given a string which contains only lowercase letters, remove duplicate letters so that every letter appear once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+Example:
+
+Given "bcabc"
+Return "abc"
+
+Given "cbacdcbc"
+Return "acdb" 
+
+'''
+
+class Solution(object):
+   def removeDuplicateLetters(self, s):
+        for c in sorted(set(s)):   # 贪心.   从最小的ch开始.  如果后面拥有所有的ch, 那么这个chJ就是结果.
+            suffix = s[s.index(c):]
+            if set(suffix) == set(s):
+                return c + self.removeDuplicateLetters(suffix.replace(c, ''))
+        return ''
+
+
+s = Solution()
+print s.removeDuplicateLetters("bcabc")
+print s.removeDuplicateLetters("cbacdcbc")
+print s.removeDuplicateLetters("cbaaaba")
+
+'''
+Given the string s, the greedy choice (i.e., the leftmost letter in the answer) is the smallest s[i], s.t. the suffix s[i .. ] contains all the unique letters. (Note that, when there are more than one smallest s[i]'s, we choose the leftmost one. Why? Simply consider the example: "abcacb".)
+
+After determining the greedy choice s[i], we get a new string s' from s by
+
+    removing all letters to the left of s[i],
+    removing all s[i]'s from s.
+
+We then recursively solve the problem w.r.t. s'.
+
+The runtime is O(26 * n) = O(n).
+'''
