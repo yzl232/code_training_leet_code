f0a2072f51d5bf27a60f5200cbe79ca8b6ee045f
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-04-15 16:05:45 -0700

    b
    b

--- Shortest Word Distance.py
+++ Shortest Word Distance.py
@@ -1,21 +1,3 @@
-'''
-Shortest Word Distance
-Total Accepted: 1754 Total Submissions: 4239 Difficulty: Easy
-
-Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
-
-For example,
-Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
-
-Given word1 = “coding”, word2 = “practice”, return 3.
-Given word1 = "makes", word2 = "coding", return 1.
-
-Note:
-You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
-
-'''
-
-
 class Solution:
     def shortestDistance(self, words, word1, word2):
         last, ret = -1, len(words)
@@ -44,4 +26,10 @@ class Solution(object):
 class Solution(object):
     def shortestDistance(self, words, word1, word2):
         return min(abs(i - j) for i in xrange(len(words)) if words[i] == word1 for j in xrange(len(words)) if words[j] == word2)
+'''
+
+
+'''
+#geeks做过。   
+Find the minimum distance between two numbers
 '''
