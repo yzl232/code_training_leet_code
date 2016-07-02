591df51dcb92185a9aefc488ea054a73b9a6d4cd
yzl232 <yzl232@Zhenglins-MacBook-Air.local>
2016-07-02 21:43:17 +0800

    x
    x

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,4 +1,13 @@
-#Determine whether an integer is a palindrome. Do this without extra space.
+class Solution:
+    # @return a boolean
+    def isPalindrome(self, x):  # just reverse integer 
+        if x<0:return False
+        y = x; rev = 0
+        while y:
+            rev, y = rev*10+y%10, y/10
+        return x==rev
+
+'''
 class Solution:
     # @return a boolean
     def isPalindrome(self, x):
@@ -9,24 +18,7 @@ class Solution:
             if x/div != x % 10:return False
             x, div = (x%div)/10, div/100
         return True
-#x的数量级也是少了100的。 去了首尾。
 
-'''
+
 It turns out that comparing from the two ends is easier. First, compare the first and last digit. If they are not the same, it must not be a palindrome. If they are the same, chop off one digit from both ends and continue until you have no digits left, which you conclude that it must be a palindrome.
-'''
-        
-'''
-class Solution:
-    # @return a boolean
-    def isPalindrome(self, x):
-        if x<0:return False
-        div = 1
-        while x / div >= 10 : div*=10
-        while x >0:
-            firstDigit = x/div
-            if firstDigit != x % 10:return False
-            x = (x%div)/10
-            div /= 100
-        return True
-        
 '''
--- java_leetcode/StringtoInteger_atoi/Solution.java
+++ java_leetcode/StringtoInteger_atoi/Solution.java
@@ -1,7 +1,20 @@
 package StringtoInteger_atoi;
 
 public class Solution {
-	public int myAtoi(String s){
-		
+	public int myAtoi(String ss){
+		String s = ss.trim()+"#";
+		int sign = 1, i=0;
+		long ret=0;
+		if(s.charAt(i)=='+') i++;
+		else if(s.charAt(i)=='-'){
+			i++;
+			sign = -1;
+		}
+		while(s.charAt(i)>='0' && s.charAt(i)<='9'){
+			ret = ret*10+s.charAt(i)-'0';
+			i++;
+			ret = sign*Math.max(Math.min(sign*ret, Integer.MAX_VALUE), Integer.MIN_VALUE);
+		}
+		return (int) (sign*ret);
 	}
 }
