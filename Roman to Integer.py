320026e6eeedf91b47b070bbce8476193de839cc
yzl232 <yzl232@Zhenglins-MacBook-Air.local>
2016-07-04 17:30:25 +0800

    z
    z

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -5,7 +5,21 @@ Input is guaranteed to be within the range from 1 to 3999..
 '''
 
 class Solution:
-    # @return an integer
+# @param {string} s
+# @return {integer}
+    def romanToInt(self, s):
+        roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+        return sum((-1 if roman[s[i]] < roman[s[i+1]] else 1) * roman[s[i]] for i in range(0, len(s) - 1))+roman[s[-1]]
+# the last letter is always added. Except the last one, if one letter is less than its latter one, this letter is subtracted.
+'''
+    def romanToInt(self, s):
+        roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+        z = 0
+        for i in range(0, len(s) - 1):
+            z += roman[s[i]] * (-1 if roman[s[i]] < roman[s[i+1]] else 1)
+        return z + roman[s[-1]]
+
+
     def romanToInt(self, s):
         one = {"I":1, "V":5, "X":10, "L":50, "C":100, "M":1000, "D":500}    # 1,  5蛮容易记忆
         two = {"IV":4, "IX":9, "XL": 40, "XC":90, "CD":400, "CM":900}   #  9,  4  容易记忆.
@@ -18,3 +32,4 @@ class Solution:
                 ret+=one[s[i]]
                 i+=1
         return ret
+'''
---
+++ java_leetcode/longestCommonPrefix/Solution.java
@@ -0,0 +1,14 @@
+package longestCommonPrefix;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        for(int i=0; i<strs[0].length(); i++){
+        	for(int j=1; j<strs.length; j++){
+        		if(!(i<strs[j].length() && strs[0].charAt(i) == strs[j].charAt(i)))
+        			return strs[0].substring(0, i);
+        	}
+        }
+        return strs[0];
+    }
+}
